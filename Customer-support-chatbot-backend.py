3cce75519877aa78cfa0e1f2704ce6cd1b45c9cf
Jérémie Kalonji <jeremie.kalonji@gmail.com>
2024-11-08 00:00:25 +0100

    adding docstrings on functions

--- Customer-support-chatbot-backend.py
+++ Customer-support-chatbot-backend.py
@@ -14,6 +14,10 @@ from statistics import mean
 
 class App1:
     def __init__(self):
+        """
+        Initialise l'application Flask et configure le cache, l'historique des accès,
+        et les modèles de chatbot pour le traitement des requêtes.
+        """
         self.app = Flask(__name__)
         self.cache = Cache(self.app, config={'CACHE_TYPE': 'flask_caching.backends.SimpleCache'})
         self.access_history = defaultdict(list)
@@ -24,11 +28,23 @@ class App1:
 
     class ChatbotAPI:
         def __init__(self, model_name, use_fp16=True):
+            """
+            Initialise le modèle de chatbot avec le nom du modèle spécifié.
+
+            :param model_name: Le nom du modèle à utiliser pour la classification.
+            :param use_fp16: Booléen indiquant si le modèle doit utiliser la précision flottante 16.
+            """
             self.reranker = FlagReranker(model_name, use_fp16=use_fp16)
             self.tag_list = ['complaint', 'refund', 'query', 'inventory', 'satisfaction']
 
         @retry(stop=stop_after_attempt(3), wait=wait_exponential(multiplier=1.5, min=4, max=10))
         def chatbot_response(self, message):
+            """
+            Génère une réponse du chatbot basée sur le message utilisateur.
+
+            :param message: Le message envoyé par l'utilisateur.
+            :return: Un json indiquant l'intention détectée du message.
+            """
             scores = []
             for e in self.tag_list:
                 score = self.reranker.compute_score([[e, message]], normalize=True)
@@ -39,6 +55,12 @@ class App1:
             return f"intent: \"{winner_tag}\""
 
     def log_status(self, f):
+        """
+        Décorateur pour enregistrer l'état des requêtes dans l'historique des accès.
+
+        :param f: La fonction à décorer.
+        :return: La fonction décorée avec un enregistrement de statut.
+        """
         @wraps(f)
         def wrapper(*args, **kwargs):
             response = f(*args, **kwargs)
@@ -55,6 +77,12 @@ class App1:
         return wrapper
 
     def check_user_message(self, message):
+        """
+        Vérifie si le message de l'utilisateur est valide selon plusieurs critères.
+
+        :param message: Le message à vérifier.
+        :return: True si le message est valide, sinon False.
+        """
         message_is_correct = False
         conditions = (
             'racist' not in message.lower() and  # Vérifie l'absence du mot "racist" (insensible à la casse)
@@ -65,23 +93,13 @@ class App1:
         return message_is_correct
 
     def count_server_error_rate_in_chat_cache(self):
-        #Permet d'obtenir les statistiques des erreurs du backend
-        # Récupère les données en cache
-        # Compte le nombre de 200 et les erreurs
-        # Retourne le taux d'erreur du backend (en pourcentage)
+        """
+        Obtient les statistiques des erreurs du backend en comptant les réponses 
+        réussies et les erreurs dans l'historique d'accès.
 
-        # Example:
-        # chat_cache = {
-        #     '/chat': [
-        #         {'status_code': 200, 'timestamp': 1673040000},
-        #         {'status_code': 404, 'timestamp': 1673040060},
-        #         {'status_code': 200, 'timestamp': 1673040090},
-        #         {'status_code': 500, 'timestamp': 1673040120}
-        #     ]
-        # }
-
-        # count_200 = 2
-        # count_error = 2
+        :return: Le taux d'erreur du backend en pourcentage.
+                 Retourne 0 si aucune donnée n'est disponible.
+        """
 
         count_200, count_error = 0, 0
         server_error_list = [404, 403, 410, 500, 503]
@@ -96,6 +114,12 @@ class App1:
         return 100*(count_error)/(count_200+count_error) if count_200+count_error > 0 else 0
 
     def background_error_rate_counter(self, interval=3):
+        """
+        Compte en arrière-plan le taux d'erreur du serveur à intervalles réguliers.
+
+        :param interval: Intervalle en secondes entre chaque comptage d'erreur.
+                         Par défaut, il est fixé à 3 secondes.
+        """
         while True:
             error_rate = self.count_server_error_rate_in_chat_cache()
             print(f"||| Server 8080 error rate: {error_rate}% |||")
@@ -104,12 +128,18 @@ class App1:
 
 
     def start_background_tasks(self):
+        """
+        Démarre les tâches d'arrière-plan pour surveiller le taux d'erreur du serveur.
+        """
         error_rate_thread = threading.Thread(target=self.background_error_rate_counter)
         error_rate_thread.daemon = True
         error_rate_thread.start()
 
 
     def setup_routes(self):
+        """
+       Configure les routes de l'application Flask pour gérer les requêtes entrantes.
+       """
         @self.app.route('/')
         @self.log_status
         def home():
@@ -194,12 +224,17 @@ class App1:
         
 
     def run(self):
+        """Démarre l'application Flask et les tâches d'arrière-plan."""
         self.setup_routes()
         self.start_background_tasks()
         self.app.run(host='0.0.0.0', port=8080)
         #self.app.run(host='0.0.0.0', port=8080, debug=True)
 
 # On crée une 2e classe App, qui correspond à l'appli de backup
+# Seules différences : 
+# #          le llm de l'appli 2 est plus léger et plus rapide
+# #          le port est 8090 (au lieu de 8080 pour l'appli 1)
+
 
 class App2:
     def __init__(self):
